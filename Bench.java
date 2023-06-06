06170b9b0447a9ca175dca55846069be9ade550e
Abe Diaz <aberoddia@gmail.com>
2023-06-05 23:50:01 -0400

    Modular multithreading

--- Bench.java
+++ Bench.java
@@ -1,14 +1,10 @@
 public class Bench{	
 
 	public static void main(String[] args){
-		Thread thread1 = new Thread(new BenchThread(), "Threading 1");
-		Thread thread2 = new Thread(new BenchThread(), "Threading 1");
-		Thread thread3 = new Thread(new BenchThread(), "Threading 1");
-		Thread thread4 = new Thread(new BenchThread(), "Threading 1");
-
-		thread1.start();
-		thread2.start();
-		thread3.start();
-		thread4.start();
+		Thread[] threads = new Thread[20];
+		for(int i = 0; i < 20; i++) threads[i] = new Thread(new BenchThread(), "Threadsss");
+		//Thread thread1 = new Thread(new BenchThread(), "Threading 1");
+		for(int j = 0; j < 20; j++) threads[j].start();
+		//thread1.start();
 	}
 }
