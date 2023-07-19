61df824ec4f12f79a4e94b10e6019b5f36394480
Abe Diaz <aberoddia@gmail.com>
2023-07-18 23:45:14 -0400

    Added aggregate functions and more join statements

---
+++ function.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(*) FROM instructor;
+SELECT SUM(credits) FROM instructor;
+SELECT AVG(salary) FROM instructor;
+SELECT MAX(salary) FROM instructor;
+SELECT MIN(salary) FROM instructor;
--- join.sql
+++ join.sql
@@ -1,5 +1,21 @@
 SELECT student.firstName, student.lastName, course.title FROM student
 JOIN takes ON student.ID = takes.ID
 JOIN course ON takes.course_id = course.course_id
-WHERE title LIKE 'Introduction to Anthropology';
+WHERE title LIKE '#DEP#';
 
+SELECT student.firstName, student.lastName FROM student
+JOIN sAddress ON student.ID = sAddress.ID
+WHERE city LIKE '#CIT#';
+
+SELECT * FROM student
+JOIN sContact ON student.ID = sContact.ID
+WHERE phone LIKE '888';
+
+SELECT instuctor.firstName, instuctor.lastName, course.title FROM student
+JOIN teaches ON instuctor.ID = teaches.ID
+JOIN course ON teaches.course_id = course.course_id
+WHERE title LIKE '#DEP#';
+
+SELECT course.title, course.credits FROM course
+JOIN class ON course.course_id = class.course_id
+WHERE semester LIKE 'Winter';
